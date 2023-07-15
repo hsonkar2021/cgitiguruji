55ba7d553382e20b16c54d545ecd23284f4fb772
hsonkar2021 <139536541+hsonkar2021@users.noreply.github.com>
2023-07-15 13:11:11 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 20px;
+}
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 50px;
+}
+
+.thumbnail {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 2.5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
+  text-align: center;
+}
+
+.thumbnail:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.thumbnail img {
+  width: 100px;
+  height: 100px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
+
+footer {
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+
+</style
+<head>
+  <title>CGITI Guruji</title>
+ 
+</head>
+<body>
+  <header>
+    <h1>Welcome to CGITI Guruji website</h1>
+	<h3>Subscribe to CGITI Guruji youtube Channel </h3>
+  </header>
+  <div class="container">
+    <div class="thumbnail">
+      <a href="mccq.php">
+        <img src="js.jpg" alt="JavaScript Quiz">
+        <h3>JavaScript MCQ Quiz</h3>
+      </a>
+    </div>
+    <div class="thumbnail">
+      <a href="python_quiz.html">
+        <img src="python.jpg" alt="Python Quiz">
+        <h3>Python MCQ Quiz</h3>
+      </a>
+    </div>
+    <div class="thumbnail">
+      <a href="java_quiz.html">
+        <img src="java.jpg" alt="Java Quiz">
+        <h3>Java MCQ Quiz</h3>
+      </a>
+    </div>
+    <div class="thumbnail">
+      <a href="vba_quiz.html">
+        <img src="vba.jpg" alt="VBA Quiz">
+        <h3>VBA MCQ Quiz</h3>
+      </a>
+    </div>
+    
+	 <div class="thumbnail">
+      <a href="cloud_computing_quiz.html">
+        <img src="mysql.jpg" alt="MySQL Quiz">
+        <h3>MySQL MCQ Quiz</h3>
+      </a>
+    </div>
+	 <div class="thumbnail">
+      <a href="cloud_computing_quiz.html">
+        <img src="ecom.jpg" alt="e Commerce Quiz">
+        <h3>e Commerce MCQ Quiz</h3>
+      </a>
+    </div>
+	 
+	 
+  </div>
+  <footer>
+    <p>&copy; 2023 Managed & Hosted by Mr.Hemant Sonkar, Training Officer, ITI Chhattisgarh. All rights reserved.</p>
+  </footer>
+</body>
+</html>
